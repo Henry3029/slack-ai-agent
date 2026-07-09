4bdfb5d81133577fbc9a280924474186f8c7012e
Henry chigozie Goodluck <chigoziehenry852@gmail.com>
2026-07-09 13:48:36 +0100

    refactor slack AI project

--- slack-backend/index.js
+++ slack-backend/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // This middleware is REQUIRED so Express can read the JSON payload shown in your screenshot
 app.use(express.json());
